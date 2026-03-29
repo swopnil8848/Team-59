6c75174061103981b801d769821094635b8ba2d0
Bijen Shrestha <shrbijen31@gmail.com>
2026-03-29 15:53:15 +0545

    revert to remove base /ai prefix

--- ai-backend/main.py
+++ ai-backend/main.py
@@ -19,15 +19,15 @@ class ReportPayload(BaseModel):
     recentGameSessions: List[Dict[str, Any]] = []
     userProfile: Dict[str, Any]
 
-@app.get("/ai/health")
+@app.get("/health")
 def health():
     return {"status": "ok", "message": "Hello Hackathon World","service":"Maybe you are looking for /docs"}
 
-@app.post("/ai/npc-questions")
+@app.post("/npc-questions")
 def npc_questions(data: PlayerData):
     return get_response(data.dict())
 
 
-@app.post("/ai/report-generate")
+@app.post("/report-generate")
 def report(data: ReportPayload):
     return get_report(data.dict())
